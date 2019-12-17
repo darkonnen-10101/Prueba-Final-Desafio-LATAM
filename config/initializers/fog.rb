a486f662ec489ee37edd9951f715a4eec1508e30
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2019-12-16 22:53:29 -0300

    f

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -10,7 +10,7 @@ else
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['AWS_KEY_CARRIERWAVE'],                        # required unless using use_iam_profile
       aws_secret_access_key: ENV['AWS_SECRET_CARRIERWAVE'],                        # required unless using use_iam_profile
-      use_iam_profile:       true,                         # optional, defaults to false
+      # use_iam_profile:       true,                         # optional, defaults to false
       # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
       # host:                  's3.example.com',             # optional, defaults to nil
       # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
